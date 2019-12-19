demining/Bitcoin-bitbox-base-Google-Colab

add: handling of state for update+shutdown+reboot
This adds the handling of the following system states to the
Supervisor:
- DOWNLOAD_UPDATE
- UPDATE_FAILED
- REBOOT
- SHUTDOWN

The Middleware logs a destinct logtag once one of these states become
active. The Supervisor watches the Middleware log and handles the
state changes by setting the respective `descriptionCodes` in the
sorted set in Redis and the respective stateCode. The Middleware is
notified that there was a state change in Redis via the named pipe
IPC, but doesn't handle the notification yet.

tools/bbbsupervisor/supervisor/eventhandling.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/digitalbitbox/bitbox-base/tools/bbbsupervisor/watcher"
 	"github.com/digitalbitbox/bitbox-base/tools/bbbsupervisor/watcher/trigger"
+	"github.com/digitalbitbox/bitbox02-api-go/api/firmware/messages"
 )
 
 /* This file includes the event parsing and handling code for the bbbsupervisor. */
@@ -19,7 +20,7 @@ func (s *Supervisor) eventLoop() {
 }
 
 // eventHandler handles errors and events.
-// When a panic occours the error is recovered from without stopping the eventLoop().
+// When a panic occurs the error is recovered from without stopping the eventLoop().
 func (s *Supervisor) eventHandler() {
 	defer func() {
 		if r := recover(); r != nil {
@@ -41,6 +42,16 @@ func (s *Supervisor) eventHandler() {
 			err = s.handleMiddlewareNoBitcoindConnectivity(event)
 		case event.Trigger == trigger.PrometheusBitcoindIBD:
 			err = s.handleBitcoindIBD(event)
+		case event.Trigger == trigger.MiddlewareBaseImageUpdateStart:
+			err = s.handleBaseImageUpdateStart(event)
+		case event.Trigger == trigger.MiddlewareBaseImageUpdateSuccess:
+			err = s.handleBaseImageUpdateSuccess(event)
+		case event.Trigger == trigger.MiddlewareBaseImageUpdateFailure:
+			err = s.handleBaseImageUpdateFailure(event)
+		case event.Trigger == trigger.MiddlewareRPCReboot:
+			err = s.handleRPCReboot(event)
+		case event.Trigger == trigger.MiddlewareRPCShutdown:
+			err = s.handleRPCShutdown(event)
 		default:
 			panic(fmt.Errorf("trigger %d is unhandled", event.Trigger))
 		}
@@ -48,6 +59,136 @@ func (s *Supervisor) eventHandler() {
 			panic(fmt.Errorf("could not trigger %s: %s", event.Trigger.String(), err))
 		}
 	}
+}
+
+// handleBaseImageUpdateStart is called when the middleware logs the logtag
+// `LogTagMWUpdateStart`. The UPDATE_FAILED state is deactivated and the
+// DOWNLOAD_UPDATE state is activated.
+func (s *Supervisor) handleBaseImageUpdateStart(event watcher.Event) error {
+	t := event.Trigger
+	err := t.IsFlooding(30*time.Second, s.state.TriggerLastExecuted[t])
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.deactivateBaseSubsystemState(messages.BitBoxBaseHeartbeatRequest_UPDATE_FAILED)
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.activateBaseSubsystemState(messages.BitBoxBaseHeartbeatRequest_DOWNLOAD_UPDATE)
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.notifyMiddlewareSubsystemStateChanged()
+	if err != nil {
+		return fmt.Errorf("could not notify the middleware about a new systemstate for trigger %q: %w", t.String(), err)
+	}
+
+	return nil
+}
+
+// handleBaseImageUpdateSuccess is called when the Middleware logs the logtag
+// `LogTagMWUpdateSuccess`. Both the UPDATE_FAILED and DOWNLOAD_UPDATE states
+// are deactivated.
+func (s *Supervisor) handleBaseImageUpdateSuccess(event watcher.Event) error {
+	t := event.Trigger
+	err := t.IsFlooding(30*time.Second, s.state.TriggerLastExecuted[t])
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.deactivateBaseSubsystemState(messages.BitBoxBaseHeartbeatRequest_UPDATE_FAILED)
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.deactivateBaseSubsystemState(messages.BitBoxBaseHeartbeatRequest_DOWNLOAD_UPDATE)
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.notifyMiddlewareSubsystemStateChanged()
+	if err != nil {
+		return fmt.Errorf("could not notify the middleware about a new systemstate for trigger %q: %w", t.String(), err)
+	}
+
+	return nil
+}
+
+// handleBaseImageUpdateFailure is called when the Middleware logs the logtag
+// `LogTagMWUpdateFailure`.The DOWNLOAD_UPDATE state is deactivated and the
+// UPDATE_FAILED state is activated.
+func (s *Supervisor) handleBaseImageUpdateFailure(event watcher.Event) error {
+	t := event.Trigger
+	err := t.IsFlooding(30*time.Second, s.state.TriggerLastExecuted[t])
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.deactivateBaseSubsystemState(messages.BitBoxBaseHeartbeatRequest_DOWNLOAD_UPDATE)
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.activateBaseSubsystemState(messages.BitBoxBaseHeartbeatRequest_UPDATE_FAILED)
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.notifyMiddlewareSubsystemStateChanged()
+	if err != nil {
+		return fmt.Errorf("could not notify the middleware about a new systemstate for trigger %q: %w", t.String(), err)
+	}
+
+	return nil
+}
+
+// handleRPCShutdown is called when the Middleware logs the logtag
+// `LogTagMWShutdown`. The SHUTDOWN state is activated. The state gets reset
+// once the Supervisor restarts.
+func (s *Supervisor) handleRPCShutdown(event watcher.Event) error {
+	t := event.Trigger
+	err := t.IsFlooding(30*time.Second, s.state.TriggerLastExecuted[t])
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.activateBaseSubsystemState(messages.BitBoxBaseHeartbeatRequest_SHUTDOWN)
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.notifyMiddlewareSubsystemStateChanged()
+	if err != nil {
+		return fmt.Errorf("could not notify the middleware about a new systemstate for trigger %q: %w", t.String(), err)
+	}
+
+	return nil
+}
+
+// handleRPCReboot is called when the Middleware logs the logtag
+// `LogTagMWReboot`. The REBOOT state gets activated. The state gets deactivated
+// once the Supervisor restarts (e.g. after the reboot).
+func (s *Supervisor) handleRPCReboot(event watcher.Event) error {
+	t := event.Trigger
+	err := t.IsFlooding(30*time.Second, s.state.TriggerLastExecuted[t])
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.activateBaseSubsystemState(messages.BitBoxBaseHeartbeatRequest_REBOOT)
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.notifyMiddlewareSubsystemStateChanged()
+	if err != nil {
+		return fmt.Errorf("could not notify the middleware about a new systemstate for trigger %q: %w", t.String(), err)
+	}
+
+	return nil
 }
 
 // handleElectrsNoBitcoindConnectivity handles the triggerElectrsNoBitcoindConnectivity

tools/bbbsupervisor/supervisor/util.go
@@ -3,10 +3,14 @@ package supervisor
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 
 	"github.com/digitalbitbox/bitbox-base/middleware/src/redis"
+	"github.com/digitalbitbox/bitbox-base/tools/bbbsupervisor/systemstate"
+	"github.com/digitalbitbox/bitbox02-api-go/api/firmware/messages"
 )
 
 // unsetClearnetIDB unsets (0 - download blocks over Tor) the ibdClearnetRedisKey if set.
@@ -82,6 +86,66 @@ func (s *Supervisor) disableBaseIBDState() (err error) {
 	err = s.setBBBConfigValue("bitcoin_ibd", "false")
 	if err != nil {
 		return fmt.Errorf("could not execute a 'bbb-config.sh set': %s", err.Error())
+	}
+
+	return nil
+}
+
+func (s *Supervisor) activateBaseSubsystemState(descCode messages.BitBoxBaseHeartbeatRequest_DescriptionCode) error {
+	err := s.redis.AddToSortedSet(redis.BaseDescriptionCode, systemstate.MapDescriptionCodePriority[descCode], strconv.Itoa(int(descCode)))
+	if err != nil {
+		return fmt.Errorf("could not activate the Base subsystem state %q: %w", descCode.String(), err)
+	}
+
+	top, err := s.redis.GetTopFromSortedSet(redis.BaseDescriptionCode)
+	if err != nil {
+		return fmt.Errorf("could not get the top element from %q: %w", redis.BaseDescriptionCode, err)
+	}
+
+	err = s.redis.SetString(redis.BaseStateCode, top)
+	if err != nil {
+		return fmt.Errorf("could not set the Base State code to %q: %w", top, err)
+	}
+
+	return nil
+}
+
+func (s *Supervisor) deactivateBaseSubsystemState(descCode messages.BitBoxBaseHeartbeatRequest_DescriptionCode) error {
+	err := s.redis.RemoveFromSortedSet(redis.BaseDescriptionCode, strconv.Itoa(int(descCode)))
+	if err != nil {
+		return fmt.Errorf("could not deactivate the Base subsystem state %q: %w", descCode.String(), err)
+	}
+
+	top, err := s.redis.GetTopFromSortedSet(redis.BaseDescriptionCode)
+	if err != nil {
+		return fmt.Errorf("could not get the top element from %q: %w", redis.BaseDescriptionCode, err)
+	}
+
+	err = s.redis.SetString(redis.BaseStateCode, top)
+	if err != nil {
+		return fmt.Errorf("could not set the Base State code to %q: %w", top, err)
+	}
+
+	return nil
+}
+
+func (s *Supervisor) notifyMiddlewareSubsystemStateChanged() error {
+	pipe, err := os.OpenFile("/tmp/middleware-notification.pipe", os.O_RDWR, 0600)
+	if err != nil {
+		return fmt.Errorf("could not notify middleware that the system state changed: %w", err)
+	}
+
+	const notification string = "{\"version\": 1, \"topic\": \"systemstate-changed\", \"payload\": {}}\n"
+
+	_, err = pipe.WriteString(notification)
+	if err != nil {
+		return fmt.Errorf("could not notify middleware that the system state changed: %w", err)
+	}
+
+	// close lets the pipe.WriteString fail and the goroutine exits
+	err = pipe.Close()
+	if err != nil {
+		return fmt.Errorf("could not close pipe: %w", err)
 	}
 
 	return nil

Tomasvrba/bitbox-base

tools/bbbsupervisor/supervisor/eventhandling.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/digitalbitbox/bitbox-base/tools/bbbsupervisor/watcher"
 	"github.com/digitalbitbox/bitbox-base/tools/bbbsupervisor/watcher/trigger"
+	"github.com/digitalbitbox/bitbox02-api-go/api/firmware/messages"
 )
 
 /* This file includes the event parsing and handling code for the bbbsupervisor. */
@@ -19,7 +20,7 @@ func (s *Supervisor) eventLoop() {
 }
 
 // eventHandler handles errors and events.
-// When a panic occours the error is recovered from without stopping the eventLoop().
+// When a panic occurs the error is recovered from without stopping the eventLoop().
 func (s *Supervisor) eventHandler() {
 	defer func() {
 		if r := recover(); r != nil {
@@ -41,6 +42,16 @@ func (s *Supervisor) eventHandler() {
 			err = s.handleMiddlewareNoBitcoindConnectivity(event)
 		case event.Trigger == trigger.PrometheusBitcoindIBD:
 			err = s.handleBitcoindIBD(event)
+		case event.Trigger == trigger.MiddlewareBaseImageUpdateStart:
+			err = s.handleBaseImageUpdateStart(event)
+		case event.Trigger == trigger.MiddlewareBaseImageUpdateSuccess:
+			err = s.handleBaseImageUpdateSuccess(event)
+		case event.Trigger == trigger.MiddlewareBaseImageUpdateFailure:
+			err = s.handleBaseImageUpdateFailure(event)
+		case event.Trigger == trigger.MiddlewareRPCReboot:
+			err = s.handleBaseReboot(event)
+		case event.Trigger == trigger.MiddlewareRPCShutdown:
+			err = s.handleBaseShutdown(event)
 		default:
 			panic(fmt.Errorf("trigger %d is unhandled", event.Trigger))
 		}
@@ -48,6 +59,141 @@ func (s *Supervisor) eventHandler() {
 			panic(fmt.Errorf("could not trigger %s: %s", event.Trigger.String(), err))
 		}
 	}
+}
+
+// handleBaseImageUpdateStart is called when the middleware logs the logtag
+// `LogTagMWUpdateStart`. The UPDATE_FAILED state is deactivated and the
+// DOWNLOAD_UPDATE state is activated.
+func (s *Supervisor) handleBaseImageUpdateStart(event watcher.Event) error {
+	t := event.Trigger
+	log.Printf("Handling trigger %q\n", t.String())
+	err := t.IsFlooding(30*time.Second, s.state.TriggerLastExecuted[t])
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.deactivateBaseSubsystemState(messages.BitBoxBaseHeartbeatRequest_UPDATE_FAILED)
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.activateBaseSubsystemState(messages.BitBoxBaseHeartbeatRequest_DOWNLOAD_UPDATE)
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.notifyMiddlewareSubsystemStateChanged()
+	if err != nil {
+		return fmt.Errorf("could not notify the middleware about a new systemstate for trigger %q: %w", t.String(), err)
+	}
+
+	return nil
+}
+
+// handleBaseImageUpdateSuccess is called when the Middleware logs the logtag
+// `LogTagMWUpdateSuccess`. Both the UPDATE_FAILED and DOWNLOAD_UPDATE states
+// are deactivated.
+func (s *Supervisor) handleBaseImageUpdateSuccess(event watcher.Event) error {
+	t := event.Trigger
+	log.Printf("Handling trigger %q\n", t.String())
+	err := t.IsFlooding(30*time.Second, s.state.TriggerLastExecuted[t])
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.deactivateBaseSubsystemState(messages.BitBoxBaseHeartbeatRequest_UPDATE_FAILED)
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.deactivateBaseSubsystemState(messages.BitBoxBaseHeartbeatRequest_DOWNLOAD_UPDATE)
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.notifyMiddlewareSubsystemStateChanged()
+	if err != nil {
+		return fmt.Errorf("could not notify the middleware about a new systemstate for trigger %q: %w", t.String(), err)
+	}
+
+	return nil
+}
+
+// handleBaseImageUpdateFailure is called when the Middleware logs the logtag
+// `LogTagMWUpdateFailure`.The DOWNLOAD_UPDATE state is deactivated and the
+// UPDATE_FAILED state is activated.
+func (s *Supervisor) handleBaseImageUpdateFailure(event watcher.Event) error {
+	t := event.Trigger
+	log.Printf("Handling trigger %q\n", t.String())
+	err := t.IsFlooding(time.Second, s.state.TriggerLastExecuted[t])
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.deactivateBaseSubsystemState(messages.BitBoxBaseHeartbeatRequest_DOWNLOAD_UPDATE)
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.activateBaseSubsystemState(messages.BitBoxBaseHeartbeatRequest_UPDATE_FAILED)
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.notifyMiddlewareSubsystemStateChanged()
+	if err != nil {
+		return fmt.Errorf("could not notify the middleware about a new systemstate for trigger %q: %w", t.String(), err)
+	}
+
+	return nil
+}
+
+// handleBaseShutdown is called when the Middleware logs the logtag
+// `LogTagMWShutdown`. The SHUTDOWN state is activated. The state gets reset
+// once the Supervisor restarts.
+func (s *Supervisor) handleBaseShutdown(event watcher.Event) error {
+	t := event.Trigger
+	log.Printf("Handling trigger %q\n", t.String())
+	err := t.IsFlooding(1*time.Second, s.state.TriggerLastExecuted[t])
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.activateBaseSubsystemState(messages.BitBoxBaseHeartbeatRequest_SHUTDOWN)
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.notifyMiddlewareSubsystemStateChanged()
+	if err != nil {
+		return fmt.Errorf("could not notify the middleware about a new systemstate for trigger %q: %w", t.String(), err)
+	}
+
+	return nil
+}
+
+// handleBaseReboot is called when the Middleware logs the logtag
+// `LogTagMWReboot`. The REBOOT state gets activated. The state gets deactivated
+// once the Supervisor restarts (e.g. after the reboot).
+func (s *Supervisor) handleBaseReboot(event watcher.Event) error {
+	t := event.Trigger
+	log.Printf("Handling trigger %q\n", t.String())
+	err := t.IsFlooding(time.Second, s.state.TriggerLastExecuted[t])
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.activateBaseSubsystemState(messages.BitBoxBaseHeartbeatRequest_REBOOT)
+	if err != nil {
+		return fmt.Errorf("could not handle trigger %q: %w", t.String(), err)
+	}
+
+	err = s.notifyMiddlewareSubsystemStateChanged()
+	if err != nil {
+		return fmt.Errorf("could not notify the middleware about a new systemstate for trigger %q: %w", t.String(), err)
+	}
+
+	return nil
 }
 
 // handleElectrsNoBitcoindConnectivity handles the triggerElectrsNoBitcoindConnectivity

tools/bbbsupervisor/supervisor/util.go
@@ -3,10 +3,14 @@ package supervisor
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 
 	"github.com/digitalbitbox/bitbox-base/middleware/src/redis"
+	"github.com/digitalbitbox/bitbox-base/tools/bbbsupervisor/systemstate"
+	"github.com/digitalbitbox/bitbox02-api-go/api/firmware/messages"
 )
 
 // unsetClearnetIDB unsets (0 - download blocks over Tor) the ibdClearnetRedisKey if set.
@@ -81,6 +85,69 @@ func (s *Supervisor) disableBaseIBDState() (err error) {
 	err = s.setBBBConfigValue("bitcoin_ibd", "false")
 	if err != nil {
 		return fmt.Errorf("could not execute a 'bbb-config.sh set': %s", err.Error())
+	}
+
+	return nil
+}
+
+func (s *Supervisor) activateBaseSubsystemState(descCode messages.BitBoxBaseHeartbeatRequest_DescriptionCode) error {
+	log.Printf("Activating the sub-system state %q\n", descCode.String())
+	err := s.redis.AddToSortedSet(redis.BaseDescriptionCode, systemstate.MapDescriptionCodePriority[descCode], strconv.Itoa(int(descCode)))
+	if err != nil {
+		return fmt.Errorf("could not activate the Base subsystem state %q: %w", descCode.String(), err)
+	}
+
+	top, err := s.redis.GetTopFromSortedSet(redis.BaseDescriptionCode)
+	if err != nil {
+		return fmt.Errorf("could not get the top element from %q: %w", redis.BaseDescriptionCode, err)
+	}
+
+	err = s.redis.SetString(redis.BaseStateCode, top)
+	if err != nil {
+		return fmt.Errorf("could not set the Base State code to %q: %w", top, err)
+	}
+
+	return nil
+}
+
+func (s *Supervisor) deactivateBaseSubsystemState(descCode messages.BitBoxBaseHeartbeatRequest_DescriptionCode) error {
+	log.Printf("Deactivating the sub-system state %q\n", descCode.String())
+	err := s.redis.RemoveFromSortedSet(redis.BaseDescriptionCode, strconv.Itoa(int(descCode)))
+	if err != nil {
+		return fmt.Errorf("could not deactivate the Base subsystem state %q: %w", descCode.String(), err)
+	}
+
+	top, err := s.redis.GetTopFromSortedSet(redis.BaseDescriptionCode)
+	if err != nil {
+		return fmt.Errorf("could not get the top element from %q: %w", redis.BaseDescriptionCode, err)
+	}
+
+	err = s.redis.SetString(redis.BaseStateCode, top)
+	if err != nil {
+		return fmt.Errorf("could not set the Base State code to %q: %w", top, err)
+	}
+
+	return nil
+}
+
+func (s *Supervisor) notifyMiddlewareSubsystemStateChanged() error {
+	log.Println("Notifying the Middleware that a sub-system state changed")
+	pipe, err := os.OpenFile("/tmp/middleware-notification.pipe", os.O_RDWR, 0600)
+	if err != nil {
+		return fmt.Errorf("could not notify middleware that a subsystem state changed: %w", err)
+	}
+
+	const notification string = "{\"version\": 1, \"topic\": \"systemstate-changed\", \"payload\": {}}\n"
+
+	_, err = pipe.WriteString(notification)
+	if err != nil {
+		return fmt.Errorf("could not notify middleware that a subsystem state changed: %w", err)
+	}
+
+	// close lets the pipe.WriteString fail and the goroutine exits
+	err = pipe.Close()
+	if err != nil {
+		return fmt.Errorf("could not close pipe: %w", err)
 	}
 
 	return nil